3637e4338078e63d38183df469e0625b4afda58b
myke47 <mychel.larson@gmail.com>
2019-06-25 14:58:12 -0700

    Add Todos Working

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { addTodo } from './actions';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: ''
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.updateNewTodo = this.updateNewTodo.bind(this);
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    this.props.addTodo({
+      value: this.state.newTodo,
+      complete: false
+    });
+    this.setState({
+      newTodo: ''
+    });
+  }
+
+  updateNewTodo(event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <form onSubmit={this.addTodo}>
+          <input
+          onChange={this.updateNewTodo}
+          placeholder='new todo'
+          value={this.state.newTodo}
+          />
+        </form>
+        <TodoList todos={this.props.todos} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps, { addTodo })(App);
